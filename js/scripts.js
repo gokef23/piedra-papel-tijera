3619e0ae590521ffc9b0140773ca0bb41e08d870
Madbunny07 <gutkevin367@gmail.com>
2023-05-15 07:40:12 -0300

    Actualizado. Juego funcional, falta cambiar la muestra de resultados

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,73 @@
-console.log('Hola mundo');
+var armaSeleccionada = "";
+
+function mostrarArma(nombreArma) {
+    document.getElementById("armaJugador").src = "icons/" + nombreArma;
+}
+
+function ocultarArma() {
+    if (armaSeleccionada === "") {
+        document.getElementById("armaJugador").src = "";
+    }
+}
+
+function checkRandoms() {
+    var lista = "";
+    for (i = 0; i < 10; i++) {
+        lista = lista.concat(Math.floor(Math.random() * 3)) + " ";
+    }
+    window.alert(lista);
+}
+
+function seleccionArmaOponente() {
+    const armas = ["Piedra", "Papel", "Tijera"];
+
+    let armaOponente = Math.floor(Math.random() * 3);
+    // window.alert("El oponente eligió " + armas[armaOponente]);
+    return armas[armaOponente];
+}
+
+function crearBoton(idElementoPadre, valorBoton, funcionOnClick) {
+    var padre = document.getElementById(idElementoPadre);
+    var boton = document.createElement("button");
+    boton.innerHTML = valorBoton;
+    boton.onclick = funcionOnClick;
+    padre.appendChild(boton);
+  }
+  
+
+function mostrarResultado(arma1, arma2, resultado) {
+    document.getElementById("Resultado").innerHTML = "El resultado es -> " + resultado + " (Jugador: " + arma1 + " / Oponente: " + arma2 + ")";
+}
+
+function jugar(armaJugador) {
+    armaSeleccionada = armaJugador;
+    var armaOponente = seleccionArmaOponente();
+    var resultado;
+    // window.alert("El jugador eligió " + armaJugador);
+    // window.alert("El oponente eligió " + seleccionArmaOponente());
+
+    // document.getElementById("armaOponente").src = "icons/" + armaOponente;
+
+    for (let element of document.getElementsByClassName("botones")) {
+        element.style.display = "none";
+    }
+
+    document.getElementById("armaJugador").src = "icons/" + armaSeleccionada + ".png";
+    document.getElementById("armaOponente").src = "icons/" + armaOponente + ".png";
+    if (armaSeleccionada === armaOponente) {
+        resultado = "Empate";
+    } else if (armaSeleccionada === "Piedra" && armaOponente === "Tijera") {
+        resultado = "Ganador: Jugador";
+    } else if (armaSeleccionada === "Papel" && armaOponente === "Piedra") {
+        resultado = "Ganador: Jugador";
+    } else if (armaSeleccionada === "Tijera" && armaOponente === "Papel") {
+        resultado = "Ganador: Jugador";
+    } else {
+        resultado = "Ganador: Oponente";
+    }
+    mostrarResultado(armaSeleccionada, armaOponente, resultado);
+
+    crearBoton("botones");
+}
+
+
